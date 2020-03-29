22ddbf725976d09fceeffd9bd7a686bdbad116d6
Luis Changir <dsl@pop-os.localdomain>
2020-03-28 23:36:34 -0400

    Cleaned some random stuff I had laying around

--- autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-feh --bg-scale $HOME/Pictures/Backgrounds/wallpaper.png &
-nm-applet &
-xscreensaver -no-splash &
-compton --config $HOME/.config/picom.conf &
-ibus-daemon -drx --panel /usr/lib/ibus/ibus-ui-gtk3 &
-clipmenud &
-
--- qtile/autostart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-feh --bg-scale ~/Pictures/Backgrounds/wallpaper.png &
-nm-applet &
-xscreensaver -no-splash &
