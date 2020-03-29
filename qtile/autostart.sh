3705e041bd27a859abaaf1049249ed66856f0f43
dotslashluis <58451056+dotslashluis@users.noreply.github.com>
2020-03-28 23:41:46 -0400

    Added comments so I remember what is what

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-feh --bg-scale $HOME/Pictures/Backgrounds/wallpaper.png &
-nm-applet &
-xscreensaver -no-splash &
-compton --config $HOME/.config/picom.conf &
-ibus-daemon -drx --panel /usr/lib/ibus/ibus-ui-gtk3 &
-clipmenud &
+feh --bg-scale $HOME/Pictures/Backgrounds/wallpaper.png & # Background
+nm-applet & # Network manager applet
+xscreensaver -no-splash & # Screen locking
+compton --config $HOME/.config/picom.conf & # Compositor
+ibus-daemon -drx --panel /usr/lib/ibus/ibus-ui-gtk3 & # iBus applet
+clipmenud & # Clipboard manager
 
